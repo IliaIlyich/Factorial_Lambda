2af6b9ee5c5817137302c41e2f6a2bbd33e94972
IliaIlyich <ikudashev93@mail.ru>
2022-03-01 21:25:37 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Factorial_Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение факториала");
+            int n = int.Parse(Console.ReadLine()); 
+            Func<int, int> func=null;
+            Console.WriteLine("Факториал числа {0}! = {1}", n, (func = n => 
+            {
+                if (n==1) return 1;
+                return n * func(n-1);   
+            })(n)); 
+        }
+    }
+}
